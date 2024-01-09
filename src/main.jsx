5f886dc3f081db88af534981c5c29f62fa436269
mh-saleh <mhsalehst@gmail.com>
2024-01-09 08:26:58 +0600

    fff

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { StaticRouter } from "react-router-dom/server.js";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StaticRouter location={`/`}>
+  <BrowserRouter>
     <App />
-  </StaticRouter>
+  </BrowserRouter>
 );
